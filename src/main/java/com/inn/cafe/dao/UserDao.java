0e5e024fcc9c239d6a823fc1dc4da9eea83f2fd3
AnupamaSandareka <gmanupama99@gmail.com>
2023-05-23 23:50:54 +0530

    user login and generate token

--- src/main/java/com/inn/cafe/dao/UserDao.java
+++ src/main/java/com/inn/cafe/dao/UserDao.java
@@ -2,7 +2,9 @@ package com.inn.cafe.dao;
 
 import com.inn.cafe.POJO.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 public interface UserDao extends JpaRepository<User,Integer> {
 
